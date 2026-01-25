d4696312bd50f35b200fe0c4fc82854105fe4254
Mitko :3 <patoka075@gmail.com>
2026-01-25 15:25:15 +0200

    Update Program.cs

--- TaskFlow.API/Program.cs
+++ TaskFlow.API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using TaskFlow.Application;
 using TaskFlow.Infrastructure;
